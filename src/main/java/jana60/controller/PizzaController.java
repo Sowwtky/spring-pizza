762de3a83bd988e5682270be67127bd0f9aab9ad
Sowwtky <miche46ale@gmail.com>
2022-07-22 16:26:28 +0200

    esercizio completo

--- src/main/java/jana60/controller/PizzaController.java
+++ src/main/java/jana60/controller/PizzaController.java
@@ -1,15 +1,23 @@
 package jana60.controller;
 
+
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jana60.model.Pizza;
 import jana60.repository.PizzaRepository;
@@ -34,13 +42,58 @@ public class PizzaController {
 	}
 	
 	 @PostMapping("/add")
-	  public String save(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult br) {
-	    if (br.hasErrors()) {
+	  public String save(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult br, Model model) {
+		 boolean hasErrors = br.hasErrors();
+		 boolean validateNome = true;
+		 if(formPizza.getId() != null) {
+			 Pizza pizzaBeforeUpdate = repo.findById(formPizza.getId()).get();
+			 if(pizzaBeforeUpdate.getNome().equals(formPizza.getNome())) {
+				 validateNome = false;
+			 }
+		}
+		 
+		 if (validateNome && repo.countByNome(formPizza.getNome()) > 0) {
+		      br.addError(new FieldError("pizza", "name", "Non ci possono essere due pizze con lo stesso nome"));
+		      hasErrors = true;
+		    }
+		 
+	    if (hasErrors) {
 	      return "/pizza/edit";
 	    } else {
-	      repo.save(formPizza);
+	    	
+	      try {
+			repo.save(formPizza);
+		} catch (Exception e) {
+			model.addAttribute("errorMessage", "Unable to save the pizza");
+			return "/pizza/edit";
+		}
+	      
 	      return "redirect:/";
 	    }
 	  }
 	
+	 @GetMapping("/delete/{id}")
+	  public String delete(@PathVariable("id") Integer pizzaId, RedirectAttributes ra) {
+	    Optional<Pizza> result = repo.findById(pizzaId);
+	    if (result.isPresent()) {
+	      repo.delete(result.get());
+	      ra.addFlashAttribute("successMessage", "Pizza " + result.get().getNome() + " eliminata!");
+	      return "redirect:/";
+	    } else {
+	      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+	          "Book con id " + pizzaId + " non presente");
+	    }
+	  }
+	 
+	 @GetMapping("/edit/{id}")
+	  public String edit(@PathVariable("id") Integer pizzaId, Model model) {
+	    Optional<Pizza> result = repo.findById(pizzaId);
+	    if (result.isPresent()) {
+	      model.addAttribute("pizza", result.get());
+	      return "/pizza/edit";
+	    } else {
+	      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+	          "Pizza con id " + pizzaId + " non presente");
+	    }
+	  }
 }
--- src/main/java/jana60/repository/PizzaRepository.java
+++ src/main/java/jana60/repository/PizzaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import jana60.model.Pizza;
 
 public interface PizzaRepository extends CrudRepository<Pizza, Integer> {
-
+	public Integer countByNome(String nome);
 }
